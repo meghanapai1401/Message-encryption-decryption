83ba3092fc201c86b20583b263bf9003c5757101
meghanapai1401 <84444385+meghanapai1401@users.noreply.github.com>
2021-05-19 19:36:31 +0530

    Add files via upload

---
+++ Encode.py
@@ -0,0 +1,41 @@
+#encode
+
+
+
+shift = int(input('enter the shift:'))
+
+print('note:give a space after each letter')
+
+data= input('enter the data:')
+
+
+
+for i in data:
+
+    if i==' ':
+
+        print('', end='')
+
+    else:
+
+        if i.isupper():
+
+            if (ord(i)+shift) < 91:
+
+                print(chr(ord(i)+shift), end='')
+
+            elif (ord(i)+shift) >= 91:
+
+                print(chr(ord(i)+shift-26), end='')
+
+        elif i.islower():
+
+            if (ord(i)+shift) < 123:
+
+                print(chr(ord(i)+shift), end='')
+
+            elif (ord(i)+shift) >= 123:
+
+                print(chr(ord(i)+shift-26), end='')
+
+                
